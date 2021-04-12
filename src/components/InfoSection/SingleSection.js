2010d730a9ea6a252ef0a84e87dae3323fe3b8b3
rashawn doyley <rashawndoyley@gmail.com>
2021-04-12 12:02:39 -0400

    new update

---
+++ src/components/InfoSection/SingleSection.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import {Container,Button} from "../../Global"
+import{  InfoSec,
+    InfoRow,
+    InfoColumn,
+    TextWrapper,
+    TopLine,
+    Heading,
+    Subtitle,
+    ImgWrapper,
+    Img} from './infoSection.styles'
+import {Link}from "react-router-dom";
+const SingleSection = ({
+    primary,
+    lightBg,
+    topLine,
+    lightTopLine,
+    lightText,
+    lightTextDesc,
+    headline,
+    description,
+    buttonLabel,
+    img,
+    alt,
+    imgStart,
+    start
+
+}) => {
+    console.log(img)
+    return (
+        <>
+            <InfoSec lightBg={lightBg}>
+                <Container>
+                <InfoRow imgStart={imgStart}>
+            <InfoColumn>
+              <TextWrapper>
+                <TopLine lightTopLine={lightTopLine}>{topLine}</TopLine>
+                <Heading lightText={lightText}>{headline}</Heading>
+                <Subtitle lightTextDesc={lightTextDesc}>{description}</Subtitle>
+                <Link to='/sign-up'>
+                  <Button big fontBig primary={primary}>
+                    {buttonLabel}
+                  </Button>
+                </Link>
+              </TextWrapper>
+            </InfoColumn>
+            
+          </InfoRow>
+                </Container>
+            </InfoSec>
+        </>
+    )
+}
+
+export default InfoSection
