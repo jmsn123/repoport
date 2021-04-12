2bb3a4cd15bd618a9429ca485f66dbb775218527
rashawn doyley <rashawndoyley@gmail.com>
2021-04-12 11:40:55 -0400

    make it better

---
+++ src/pages/Contact/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import{InfoSection}from "../../components"
+import{homeObjOne} from './data'
+const Home = () => {
+    return (
+        <>
+                <InfoSection {...homeObjOne}/>
+              
+
+
+        </>
+    )
+}
+
+export default Contact
