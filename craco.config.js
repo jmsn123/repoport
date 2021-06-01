cda63f696e04f8d24672d350a3c50a8748520f3b
rashawn <rashawndoyley@gmail.com>
2021-06-01 11:36:32 -0400

    nice commit

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,18 @@
+let target = "web";
+if (process.env.REACT_APP_MODE === "electron") {
+  target = "electron-renderer";
+}
+console.log(`craco.config.js: setting webpack target to: ${target}`);
+
 module.exports = {
   style: {
     postcss: {
       plugins: [require("tailwindcss"), require("autoprefixer")],
+    },
+  },
+  webpack: {
+    configure: {
+      target: target,
     },
   },
 };
